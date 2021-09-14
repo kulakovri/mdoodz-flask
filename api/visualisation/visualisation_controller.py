ae611bf98806b063794481d726d647144484d973
Roman Kulakov <kulakovri@gmail.com>
2021-09-14 18:55:24 +0300

    Visualisation: add controller to show model

--- api/visualisation/visualisation_controller.py
+++ api/visualisation/visualisation_controller.py
@@ -1,11 +1,21 @@
-from flask import Blueprint, request
-from .visualisation_service import run_visualisation
+from flask import Blueprint, send_file, request
+from . import visualisation_service
 
 blueprint = Blueprint('visualisation', __name__)
+blueprint_images = Blueprint('get-image', __name__)
 
 
-@blueprint.get('/api/visualise')
+@blueprint.post('/api/visualise')
 def visualise():
-  print('visualising')
-  run_visualisation()
+  visualisation_service.run_visualisation()
   return ''
+
+
+@blueprint_images.get('/api/images')
+def get_image():
+  number = request.args.get('number')
+  if number is None:
+    return visualisation_service.get_image_count()
+  else:
+    endcoded_file = visualisation_service.get_image(number)
+    return send_file(endcoded_file, mimetype='image/png')
--- api/visualisation/visualisation_service.py
+++ api/visualisation/visualisation_service.py
@@ -1,47 +1,59 @@
+import io
+
 import h5py as h5py
 import numpy as np
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import os
 
+images_dir = 'images'
+mdoodz_source_dir = 'mdoodz_source'
+
+
+def get_image(number: str):
+  file_path = os.path.join(images_dir, f'{number}.png')
+  file = open(file_path, "rb")
+  return file
+
+
+def get_image_count():
+  files = os.listdir(images_dir)
+  return str(len(files))
+
 
 def run_visualisation():
   output_files = _get_list_of_output_files()
-
+  count = 0
   for file_name in output_files:
-    file = h5py.File(f'mdoodz_source/{file_name}', 'r')
+    file = h5py.File(f'{mdoodz_source_dir}/{file_name}', 'r')
 
-    P = file['Centers/P']
-    T = file['Centers/T']
-    xc = file['/Model/xc_coord']
-    zc = file['/Model/zc_coord']
     data = file['/Model/Params']
-    data[0]
-    data[1]
-    data[2]
     Nx = data[3].astype(int)
     Nz = data[4].astype(int)
     Ncx = Nx - 1
     Ncz = Nz - 1
 
-    P = np.reshape(P, (Ncz, Ncx))
-    T = np.reshape(T, (Ncz, Ncx))
-
     fig = Figure()
     canvas = FigureCanvas(fig)
+
     pressure_ax = fig.add_subplot(211)
+    P = file['Centers/P']
+    P = np.reshape(P, (Ncz, Ncx))
     pcm = pressure_ax.pcolormesh(P, cmap='gist_heat')
     fig.colorbar(pcm)
 
     temperature_ax = fig.add_subplot(212)
+    T = file['Centers/T']
+    T = np.reshape(T, (Ncz, Ncx))
     tcm = temperature_ax.pcolormesh(T, cmap='gist_heat')
     fig.colorbar(tcm)
 
-    canvas.print_figure(f'{file_name}.png')
+    canvas.print_figure(f'{images_dir}/{count}.png')
+    count += 1
 
 
 def _get_list_of_output_files():
-  files = os.listdir('mdoodz_source')
+  files = os.listdir(f'{mdoodz_source_dir}')
   output_files = list(filter(lambda file: _filter_output_files(file), files))
   return sorted(output_files)
 
